a88db73be434f8a9cd6167755901314f8d66d1a8
Megaax <mohamedxmagdi@outlook.com>
2024-02-20 14:33:45 +0200

    fix chromedriver

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -11,6 +11,90 @@
 //             __/ |
 //            |___/
 
+// module.exports = {
+//   // An array of folders (excluding subfolders) where your tests are located;
+//   // if this is not specified, the test source must be passed as the second argument to the test runner.
+//   src_folders: ['test'],//,'nightwatch/examples'],
+
+//   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
+//   page_objects_path: ['page-objects'],
+
+//   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
+//   custom_commands_path: ['nightwatch/custom-commands'],
+
+//   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
+//   custom_assertions_path: ['nightwatch/custom-assertions'],
+
+//   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
+//   plugins: [],
+
+//   // See https://nightwatchjs.org/guide/concepts/test-globals.html
+//   globals_path: '',
+
+//   webdriver: {},
+
+//   test_workers: {
+//     enabled: true
+//   },
+
+//   test_settings: {
+//     default: {
+//       disable_error_log: false,
+//       launch_url: 'http://localhost',
+
+//       screenshots: {
+//         enabled: false,
+//         path: 'screens',
+//         on_failure: true
+//       },
+
+//       desiredCapabilities: {
+//         browserName: 'chrome'
+//       },
+
+//       webdriver: {
+//         start_process: true,
+//         server_path: ''
+//       },
+
+//     },
+
+//     chrome: {
+//       desiredCapabilities: {
+//         browserName: 'chrome',
+//         'goog:chromeOptions': {
+//           // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
+//           //
+//           // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
+//           w3c: true,
+//           args: [
+//             //'--no-sandbox',
+//             //'--ignore-certificate-errors',
+//             //'--allow-insecure-localhost',
+//             //'--headless'
+//           ]
+//         }
+//       },
+
+//       webdriver: {
+//         start_process: true,
+//         server_path: '',
+//         cli_args: [
+//           // --verbose
+//         ]
+//       }
+//     },
+
+//   },
+
+//   usage_analytics: {
+//     enabled: true,
+//     log_path: './logs/analytics',
+//     client_id: '7d1fb87c-ff1d-421e-8577-e49f2ce6ce0e'
+//   }
+
+// };
+// nightwatch.config.js
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
@@ -56,7 +140,6 @@ module.exports = {
         start_process: true,
         server_path: ''
       },
-
     },
 
     chrome: {
@@ -85,6 +168,22 @@ module.exports = {
       }
     },
 
+    firefox: {
+      desiredCapabilities: {
+        browserName: 'firefox',
+        'moz:firefoxOptions': {
+          // Firefox-specific options
+        }
+      },
+
+      webdriver: {
+        start_process: true,
+        server_path: '',
+        cli_args: [
+          // --verbose
+        ]
+      }
+    },
   },
 
   usage_analytics: {
@@ -92,5 +191,4 @@ module.exports = {
     log_path: './logs/analytics',
     client_id: '7d1fb87c-ff1d-421e-8577-e49f2ce6ce0e'
   }
-
 };
